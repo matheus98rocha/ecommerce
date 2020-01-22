8cf341647d25b3290961c55dfeb04f136dadf9fb
Matheus Gonçalves Rocha <matheus98rocha@gmail.com>
2020-01-21 23:35:08 -0300

    Recuperar senha

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ $app->post("/admin/users/create",function(){
 
 	$user = new User();
 
-	$_POST["inadmin"] = (isset($_POST[":inadmin"]))?1:0;
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
 
 	$user->setData($_POST);
 
@@ -132,7 +132,7 @@ $app->post("/admin/users/:iduser",function($iduser){
 
 	$user = new User();
 
-	$_POST["inadmin"] = (isset($_POST[":inadmin"]))?1:0;
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
 
 	$user->get((int)$iduser);
 
@@ -145,6 +145,81 @@ $app->post("/admin/users/:iduser",function($iduser){
 
 });
 
+$app->get("/admin/forgot", function(){
+
+		$page = new PageAdmin
+		([
+		"header"=>false,
+		"footer"=>false
+		]);
+
+	$page->setTpl("forgot");
+
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+
+$app->get("/admin/forgot/sent", function(){
+	$page = new PageAdmin
+		([
+		"header"=>false,
+		"footer"=>false
+		]);
+
+	$page->setTpl("forgot-sent");
+
+});
+
+$app->get("/admin/forgot/reset", function(){
+	
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin
+		([
+		"header"=>false,
+		"footer"=>false
+		]);
+
+	$page->setTpl("forgot-reset",array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+
+});
+
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User:: setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT,[
+	"cost"=>12
+	]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin
+		([
+		"header"=>false,
+		"footer"=>false
+		]);
+
+	$page->setTpl("forgot-reset-sucess");
+
+
+
+});
 
 $app->run();
 

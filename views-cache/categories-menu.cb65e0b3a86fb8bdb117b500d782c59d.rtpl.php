fb1c47797ce4ee83fdc1074237aa4eba471d1122
Matheus Gonçalves Rocha <matheus98rocha@gmail.com>
2020-01-25 17:51:13 -0300

    Site - categorias

---
+++ views-cache/categories-menu.cb65e0b3a86fb8bdb117b500d782c59d.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/4">Android</a></li><li><a href="/categories/3">Eletronico</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/5">Samsung</a></li>
